e6d1f5a0e2a5f672cb5e01614adc75d294eabca5
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-03-28 15:18:39 +0300

    calculator-web-app
    Halfway done JS implemented functionality

---
+++ script.js
@@ -0,0 +1,63 @@
+function addition(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+let firstNum = '';
+let operation = '';
+let secondNum = '';
+const screen = document.querySelector(".display-screen");
+const digits = document.querySelectorAll(".digit");
+/*Previously the for each loop was inside the  function updateFirstNum but that
+would result in the loop add eventListener to all the button every time they
+are clicked so we took it outside here plus we dont have to do this updateFirstNUm()*/
+digits.forEach((digit) => {
+    digit.addEventListener("click", () => {
+         if (operation !== ''){
+            updateSecondName();
+         } else if (operation === ''){
+            updateFirstNum();
+         };
+    });
+});
+const clearBtn = document.querySelector(".clear-button");
+clearBtn.addEventListener("click", () => {
+    screen.textContent = "";
+})
+
+function operate(operator, num1, num2){
+    
+    if (operator === "+"){
+        return addition(num1, num2);
+    } else if (operator === "-"){
+        return subtract(num1, num2);
+    }  else if (operator === "*"){
+        return multiply(num1, num2);
+    } else if (operator === "/"){
+        return divide(num1, num2)
+    };
+}
+
+function updateFirstNum(e){
+    firstNum += e.target.value;
+    screen.textContent = firstNum;
+}
+
+function updateSecondName(e){
+    secondNum += e.target.value
+    screen.textContent = `${firstNum} ${secondNum}`
+}
+
+
+
